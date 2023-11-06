01b557e1bf0ad2846163b4aa155d60a36cc35016
tliam1 <97718694+tliam1@users.noreply.github.com>
2023-11-05 15:47:20 -1000

    Update fixed-point.h

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -4,18 +4,6 @@ in the kernel, This means that calculations on real quantities must
 be simulated using integers. The fundamental idea is to treat the rightmost
 bits of an integer as representing a Fraction.
 
-we can designate the lowest 14 bits of a signed 32-bit integer as
-Fractional bits, This is called a 17.14 fixed-point number.
-
-Suppose that we are using a p.q fixed-point format, and let f = 2q. By the
-definition above, we can convert an integer or real number into p.q format by
-multiplying with f.
-
-To convert a fixed-point value back to an integer, divide by f.
-The normal ‘/’ operator in C rounds toward zero, that is, it rounds
-positive numbers down and negative numbers up. To round to nearest, add f /2 to a positive
-number, or subtract it from a negative number, before dividing.
-
 Let x and y be fixed-point numbers, and let n be an integer.
 Then the sum of x and y is x + y
 and their difference is x - y.
